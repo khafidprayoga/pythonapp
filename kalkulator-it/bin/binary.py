8cffa8e5fb63da73fb5a1818cfdf81bb73545aee
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-13 20:33:14 +0700

    kalkulator-it add bin converter

---
+++ kalkulator-it/bin/binary.py
@@ -0,0 +1,11 @@
+def to_dec(number):
+    """Convert binary to decimal"""
+    return int(number, 2)
+
+def to_oct(number):
+    """Convert binary to octal"""
+    return oct(to_dec(number))[2:]
+
+def to_hex(number):
+    """Convert binary to hexadecimal"""
+    return hex(to_dec(number))[2:]
