232a7d9e7a836ed20dabb9c12e80f81d01060059
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-13 20:34:01 +0700

    kalkulator-it: add setup.py

--- kalkulator-it/setup.py
+++ kalkulator-it/setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='calculator',
-    version='0.1',
-    package=find_packages(),
-    include_package_data=True,
-    install_requires=[
+    name = 'calculator',
+    version = '0.1',
+    author = 'Khafid Prayoga',
+    author_email = 'khafid.dev@gmail.com',
+    description = ('Simple calculator for CS students.'),
+    package = find_packages(),
+    include_package_data = True,
+    install_requires = [
         'Click',
     ],
-    entry_points='''
+    entry_points = '''
         [console_scripts]
         calculate=main:calculate
     '''
