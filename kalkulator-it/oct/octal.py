4e3f7424ca4b7f4250d219c901f490443cb2e704
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-13 20:33:23 +0700

    kalkulator-it add oct converter

---
+++ kalkulator-it/oct/octal.py
@@ -0,0 +1,11 @@
+def to_dec(number):
+    """Convert octal to decimal"""
+    return int(number, 8)
+
+def to_bin(number):
+    """Convert octal to binary"""
+    return bin(to_dec(number))[2:]
+
+def to_hex(number):
+    """Convert octal to hexadecimal"""
+    return hex(to_dec(number))[2:]
