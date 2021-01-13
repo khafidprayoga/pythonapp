6d0f232c22eb34d70903fdacf62bffcf6f2d323b
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-13 20:00:15 +0700

    kalkulator-it: add decimal converter

---
+++ kalkulator-it/dec/decimal.py
@@ -0,0 +1,11 @@
+def to_bin(number):
+    """Convert decimal to binary"""
+    return bin(number)[2:]
+
+def to_oct(number):
+    """Convert decimal to octal"""
+    return oct(number)[2:]
+
+def to_hex(number):
+    """Convert decimal to hexadecimal"""
+    return hex(number)[2:]
