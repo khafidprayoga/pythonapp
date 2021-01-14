e6f0e8a04e082b99c6aa9d88726d5649d332d55d
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-14 10:37:34 +0700

    kalkulator-it: add try exception

--- kalkulator-it/main.py
+++ kalkulator-it/main.py
@@ -16,50 +16,53 @@ def calculate(source, destination, number):
     """
     Simple program that converts computer number.
     """
-    if source == 'bin':
-        if destination == 'dec':
-            click.echo(f'Result\t: {binary.to_dec(number)}')
-        elif destination == 'oct':
-            click.echo(f'Result\t: {binary.to_oct(number)}')
-        elif destination == 'hex':
-            click.echo(f'Result\t: {binary.to_hex(number)}')
-        else:
-            click.echo('Destination error, not a valid value.')
-            sys.exit(2)
+    try:
+        if source == 'bin':
+            if destination == 'dec':
+                click.echo(f'Result\t: {binary.to_dec(number)}')
+            elif destination == 'oct':
+                click.echo(f'Result\t: {binary.to_oct(number)}')
+            elif destination == 'hex':
+                click.echo(f'Result\t: {binary.to_hex(number)}')
+            else:
+                click.echo('Destination error, not a valid value.')
+                sys.exit(2)
 
-    elif source == 'dec':
-        number = int(number)
-        if destination == 'bin':
-            click.echo(f'Result\t: {decimal.to_bin(number)}')
-        elif destination == 'oct':
-            click.echo(f'Result\t: {decimal.to_oct(number)}')
-        elif destination == 'hex':
-            click.echo(f'Result\t: {decimal.to_hex(number)}')
-        else:
-            click.echo('Destination error, not a valid value.')
-            sys.exit(2)
+        elif source == 'dec':
+            number = int(number)
+            if destination == 'bin':
+                click.echo(f'Result\t: {decimal.to_bin(number)}')
+            elif destination == 'oct':
+                click.echo(f'Result\t: {decimal.to_oct(number)}')
+            elif destination == 'hex':
+                click.echo(f'Result\t: {decimal.to_hex(number)}')
+            else:
+                click.echo('Destination error, not a valid value.')
+                sys.exit(2)
 
-    elif source == 'oct':
-        if destination == 'bin':
-            click.echo(f'Result\t: {octal.to_bin(number)}')
-        elif destination == 'dec':
-            click.echo(f'Result\t: {octal.to_dec(number)}')
-        elif destination == 'hex':
-            click.echo(f'Result\t: {octal.to_hex(number)}')
-        else:
-            click.echo('Destination error, not a valid value.')
-            sys.exit(2)
+        elif source == 'oct':
+            if destination == 'bin':
+                click.echo(f'Result\t: {octal.to_bin(number)}')
+            elif destination == 'dec':
+                click.echo(f'Result\t: {octal.to_dec(number)}')
+            elif destination == 'hex':
+                click.echo(f'Result\t: {octal.to_hex(number)}')
+            else:
+                click.echo('Destination error, not a valid value.')
+                sys.exit(2)
 
-    elif source == 'hex':
-        if destination == 'bin':
-            click.echo(f'Result\t: {hexdec.to_bin(number)}')
-        elif destination == 'dec':
-            click.echo(f'Result\t: {hexdec.to_dec(number)}')
-        elif destination == 'oct':
-            click.echo(f'Result\t: {hexdec.to_oct(number)}')
+        elif source == 'hex':
+            if destination == 'bin':
+                click.echo(f'Result\t: {hexdec.to_bin(number)}')
+            elif destination == 'dec':
+                click.echo(f'Result\t: {hexdec.to_dec(number)}')
+            elif destination == 'oct':
+                click.echo(f'Result\t: {hexdec.to_oct(number)}')
+            else:
+                click.echo('Destination error, not a valid value.')
+                sys.exit(2)
         else:
-            click.echo('Destination error, not a valid value.')
-            sys.exit(2)
-    else:
-        click.echo('Source error, not a valid value.')
-        sys.exit(1)
+            click.echo('Source error, not a valid value.')
+            sys.exit(1)
+    except ValueError as err:
+        click.echo('Error: %s ' % err)
